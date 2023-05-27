ac2642cd08e742b3847cb8f3cc4021d941d02771
Pedro Zattoni Scroccaro <pedroszattoni@users.noreply.github.com>
2023-05-27 18:35:58 +0200

    Update __init__.py

--- src/invopt/__init__.py
+++ src/invopt/__init__.py
@@ -1,4 +1,5 @@
 from .main import (dec_to_bin, ASL, evaluate, discrete_consistent, discrete,
+                   continuous_linear, continuous_quadratic,
                    mixed_integer_linear, mixed_integer_quadratic, FOM)
 
 __all__ = ['dec_to_bin',
@@ -6,6 +7,8 @@ __all__ = ['dec_to_bin',
            'evaluate',
            'discrete_consistent',
            'discrete',
+           'continuous_linear',
+           'continuous_quadratic',
            'mixed_integer_linear',
            'mixed_integer_quadratic',
            'FOM',
