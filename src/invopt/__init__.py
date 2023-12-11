9a7d2590c2bcfbfc57a8213722b7a3ca7dc9b4ef
Pedro Zattoni Scroccaro <pedroszattoni@users.noreply.github.com>
2023-12-11 14:13:44 +0100

    Update __init__.py with candidate_action function

--- src/invopt/__init__.py
+++ src/invopt/__init__.py
@@ -1,15 +1,19 @@
-from .main import (dec_to_bin, ASL, evaluate, discrete_consistent, discrete,
-                   continuous_linear, continuous_quadratic,
-                   mixed_integer_linear, mixed_integer_quadratic, FOM)
+from .main import (
+    dec_to_bin, candidate_action, ASL, evaluate, discrete_consistent, discrete,
+    continuous_linear, continuous_quadratic, mixed_integer_linear,
+    mixed_integer_quadratic, FOM
+)
 
-__all__ = ['dec_to_bin',
-           'ASL',
-           'evaluate',
-           'discrete_consistent',
-           'discrete',
-           'continuous_linear',
-           'continuous_quadratic',
-           'mixed_integer_linear',
-           'mixed_integer_quadratic',
-           'FOM',
-           ]
+__all__ = [
+    'dec_to_bin',
+    'candidate_action',
+    'ASL',
+    'evaluate',
+    'discrete_consistent',
+    'discrete',
+    'continuous_linear',
+    'continuous_quadratic',
+    'mixed_integer_linear',
+    'mixed_integer_quadratic',
+    'FOM',
+]
